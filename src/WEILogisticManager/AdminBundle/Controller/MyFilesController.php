b11609b15fcef398bff030a61670b7693084c134
cesarBere <c.berezowski@gmail.com>
2013-10-29 16:32:17 +0100

    Properly recreated MyFilesController

--- src/WEILogisticManager/AdminBundle/Controller/MyFilesController.php
+++ src/WEILogisticManager/AdminBundle/Controller/MyFilesController.php
@@ -1,17 +1,8 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Czoo
- * Date: 29/10/13
- * Time: 16:09
- * To change this template use File | Settings | File Templates.
- */
 
 namespace WEILogisticManager\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-// these import the "@Route" and "@Template" annotations
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
@@ -25,4 +16,5 @@ class MyFilesController extends Controller
     {
         return $this->render('WEILogisticManagerAdminBundle:MyFiles:index.html.twig');
     }
+
 }
