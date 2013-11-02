d1d6e606dc5babb87201cd9139f3493ff193f250
cesarBere <c.berezowski@gmail.com>
2013-11-02 15:19:25 +0100

    Added a "create event" page + form

--- src/WEILogisticManager/AdminBundle/Controller/HomepageController.php
+++ src/WEILogisticManager/AdminBundle/Controller/HomepageController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 // these import the "@Route" and "@Template" annotations
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use WEILogisticManager\AdminBundle\Entity\Event;
 
 class HomepageController extends Controller
 {
@@ -14,7 +16,7 @@ class HomepageController extends Controller
      * @Route("/homepage")
      * @Template()
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $qb = $em->createQueryBuilder();
@@ -23,8 +25,40 @@ class HomepageController extends Controller
         $qb->select('e');
         $events = $qb->getQuery()->getResult();
 
+
+
         return $this->render('WEILogisticManagerAdminBundle:Homepage:index.html.twig', array(
-            'events' => $events
+            'events' => $events,
+        ));
+    }
+
+    /**
+     * @Route("/homepage/create_event")
+     * @Template()
+     */
+    public function createEventAction(Request $request)
+    {
+        $event = new Event();
+        $event->setName('test');
+        $event->setPlace('paris');
+
+        $form = $this->createFormBuilder($event)
+            ->add('name', 'text')
+            ->add('beginDate', 'date')
+            ->add('endDate', 'date')
+            ->add('place', 'text')
+            ->add('create', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isValid())
+        {
+            //Persist object in database
+        }
+
+        return $this->render('WEILogisticManagerAdminBundle:Homepage:createEvent.html.twig', array(
+            'form' => $form->createView(),
         ));
     }
 }
