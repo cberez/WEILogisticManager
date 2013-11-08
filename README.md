b4a28a986cdd5381d13decd5a0ab574c599424af
cesarBere <c.berezowski@gmail.com>
2013-11-08 17:27:14 +0100

    Added build status in README, reorganized test files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 WEILogisticManager
 ================== 
+
+[![Build Status](https://travis-ci.org/cesarBere/WEILogisticManager.png?branch=master)](https://travis-ci.org/cesarBere/WEILogisticManager)
--- src/WEILogisticManager/AdminBundle/Tests/Controller/HomepageControllerTest.php
+++ src/WEILogisticManager/AdminBundle/Tests/Controller/HomepageControllerTest.php
@@ -4,14 +4,13 @@ namespace WEILogisticManager\AdminBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class HomepageControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $crawler = $client->request('GET', '/activities');
     }
+
 }
